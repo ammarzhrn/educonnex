f368eba0dbc48576ce9923d35ea4f2bfe620917a
LeBrontoJames <keenanabigailidn@gmail.com>
2024-10-15 15:50:16 +0700

    database

---
+++ database/migrations/2024_10_14_092219_create_programs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_sector')->unsigned();
+            $table->string('title');
+            $table->text('desc');
+            $table->string('proposal_link');
+            $table->string('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programs');
+    }
+};
